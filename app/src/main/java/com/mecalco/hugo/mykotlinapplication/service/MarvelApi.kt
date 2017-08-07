109a0350f50ee387d524aa5b8b6197a8409345f4
hugoM1 <hugo.mecalco@accedo.tv>
2017-08-07 16:20:59 -0500

    MarvelApi minor fixes.

--- app/src/main/java/com/mecalco/hugo/mykotlinapplication/service/MarvelApi.kt
+++ app/src/main/java/com/mecalco/hugo/mykotlinapplication/service/MarvelApi.kt
@@ -15,23 +15,23 @@ interface MarvelApi {
     @GET("characters?orderBy=-modified&ts=1&hash=b68e7aeef8e1843eb3b2958aa737c743")
     fun getCharactersList(
             @Query("apikey") apiKey: String,
-            @Query("limit") limit: Int): Call<List<Characters>>
+            @Query("limit") limit: Int): Call<Characters>
 
     //https://gateway.marvel.com/v1/public/characters/1009148?ts=1&apikey=fb9cf622de091ac20051e62a51c81149&hash=b68e7aeef8e1843eb3b2958aa737c743
     @GET("characters/{characterId}?ts=1&hash=b68e7aeef8e1843eb3b2958aa737c743")
     fun getCharacterDetail(
             @Path("characterId") characterId: Int,
-            @Query("apikey") apiKey: String): Call<List<Characters>>
+            @Query("apikey") apiKey: String): Call<Characters>
 
     //https://gateway.marvel.com/v1/public/characters/1010338/comics?ts=1&apikey=fb9cf622de091ac20051e62a51c81149&hash=b68e7aeef8e1843eb3b2958aa737c743
     @GET("characters/{characterId}/comics?ts=1&hash=b68e7aeef8e1843eb3b2958aa737c743")
     fun getComicsByCharacter(
             @Path("characterId") characterId: Int,
-            @Query("apikey") apiKey: String): Call<List<Comics>>
+            @Query("apikey") apiKey: String): Call<Comics>
 
     //https://gateway.marvel.com/v1/public/comics/61431?ts=1&apikey=fb9cf622de091ac20051e62a51c81149&hash=b68e7aeef8e1843eb3b2958aa737c743
     @GET("comics/{comicId}?ts=1&hash=b68e7aeef8e1843eb3b2958aa737c743")
     fun getComicDetail(
             @Path("comicId") comicId: Int,
-            @Query("apikey") apiKey: String): Call<List<Comics>>
+            @Query("apikey") apiKey: String): Call<Comics>
 }
